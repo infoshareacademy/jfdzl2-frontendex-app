9327646effa29493c20abba2e28aaf9583a1ad68
Agnieszka Koryl <ag.koryl@gmail.com>
2019-02-10 13:56:15 +0100

    Form

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,33 +2,16 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 
-const styles = theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-    },
-    dense: {
-        marginTop: 16,
-    },
-    menu: {
-        width: 200,
-    },
-});
-
 class Search extends React.Component {
 
-
-
     render() {
+
         return (
 
             <form>
                 <h2>Szukaj mechanika</h2>
                 <TextField
+        
                     required
                     id=""
                     defaultValue="Lokalizacja"
@@ -36,6 +19,7 @@ class Search extends React.Component {
                     variant="outlined"
                 />
                    <TextField
+                  
                     required
                     id=""
                     defaultValue="Nazwa firmy"
@@ -43,6 +27,7 @@ class Search extends React.Component {
                     variant="outlined"
                 />
                    <TextField
+                   
                     required
                     id=""
                     defaultValue="Specjalizacja"
@@ -50,6 +35,7 @@ class Search extends React.Component {
                     variant="outlined"
                 />
                    <TextField
+                 
                     required
                     id=""
                     defaultValue="Marka samochodu"
