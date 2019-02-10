7db17ad279df12a2dbdff0571325cf9d15216a62
Agnieszka Koryl <ag.koryl@gmail.com>
2019-02-10 12:33:24 +0100

    Structure added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Main from './routes/Main'; 
+import Main from './routes/Main/Main'; 
 
 class App extends Component {
   render() {
