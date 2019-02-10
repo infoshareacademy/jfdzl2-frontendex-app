c7d654216a63a9b89a42e17f64f0d81419b8a28c
Kursant <kursant@kursant.pl>
2019-02-10 13:40:30 +0100

    First mechanic added to database

---
+++ src/database/Database.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class ApplicationBar extends React.Component {
+	state = {
+    todos: [
+      {
+        id: 1,
+        name:  "Bogdan Auto",
+				city: "Lublin",
+				street: "Hirszfelda",
+				carBrand: "Audi",
+				carModel: "A4",
+      }, 
+    ]
+  }
+	render() {
+		return (
+			<div>  
+
+			</div>
+		)
+	}
+} 
+
+export default Database;
