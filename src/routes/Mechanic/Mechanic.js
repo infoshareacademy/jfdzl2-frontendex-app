21fe55d9f30e6665dc672bb4eab2db7eb1bfb5ed
Kursant <kursant@kursant.pl>
2019-02-17 12:50:12 +0100

    Tab content

--- src/routes/Mechanic/Mechanic.js
+++ src/routes/Mechanic/Mechanic.js
@@ -1,8 +1,23 @@
 import React, {Component} from 'react';
 
+import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+
+import Typography from '@material-ui/core/Typography';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 
 class Mechanic extends Component{ 
@@ -24,15 +39,15 @@ class Mechanic extends Component{
 
 				<AppBar position="static">
           <Tabs value={value} onChange={this.handleChange}>
-            <Tab label="Item One" />
-            <Tab label="Item Two" />
-            <Tab label="Item Three" />
+            <Tab label="Usługi" />
+            <Tab label="Lokalizacja" />
+            <Tab label="Ocena" />
           </Tabs>
         </AppBar>
 
-				<div> Usługi </div>
-				<div> Ocena</div>
-				<div> Adres</div>
+				{value === 0 && <TabContainer>Usługi</TabContainer>}
+        {value === 1 && <TabContainer>Lokalizacja</TabContainer>}
+        {value === 2 && <TabContainer>Adres</TabContainer>} 
 
 				<div> Footer </div>
 			</React.Fragment>
