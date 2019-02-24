985979de2ca0d3b428d2e7c633c089986a325331
adrian-slowinski <adrian.slowinski87@gmail.com>
2019-02-24 12:22:58 +0100

    Code reafactor on mechanic.js file

--- src/routes/Mechanic/Mechanic.js
+++ src/routes/Mechanic/Mechanic.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -14,68 +14,68 @@ import firebase from '../../database/firebase';
 
 
 function TabContainer(props) {
-  return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {props.children}
-    </Typography>
-  );
+	return (
+		<Typography component="div" style={{ padding: 8 * 3 }}>
+			{props.children}
+		</Typography>
+	);
 }
 
 TabContainer.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 };
 
 
-class Mechanic extends Component{ 
+class Mechanic extends Component {
 	state = {
-    value: 0,
-  };
-
-  handleChange = (event, value) => {
-    this.setState({ value });
+		value: 0,
 	};
 
-	componentDidMount(){
+	handleChange = (event, value) => {
+		this.setState({ value });
+	};
 
-		firebase.database.collection("brands").get().then(function(querySnapshot) {
-			querySnapshot.forEach(function(doc) {
-					// doc.data() is never undefined for query doc snapshots
-					console.log(doc.id, " => ", doc.data());
+	componentDidMount() {
+
+		firebase.database.collection("brands").get().then(function (querySnapshot) {
+			querySnapshot.forEach(function (doc) {
+				// doc.data() is never undefined for query doc snapshots
+				console.log(doc.id, " => ", doc.data());
 			});
-	});
-	
+		});
+
 
 	}
-	
-	render(){ 
+
+	render() {
 		const { classes } = this.props;
 		const { value } = this.state;
-		
-		return(
-			<React.Fragment>
-				<h1 className="mechanic__header"> Nazwa zakładu mechanicznego/Heading </h1> 
- 
-					<AppBar position="static">
-						<Tabs 
-						value={value} 
-						onChange={this.handleChange} 
-						centered
-						>
-							<Tab label="Usługi" />
-							<Tab label="Lokalizacja" />
-							<Tab label="Ocena" />
-						</Tabs>
-					</AppBar> 
 
-					<SwipeableViews  
-						index={this.state.value}
-						onChangeIndex={this.handleChangeIndex}
+		return (
+			<React.Fragment>
+				<h1 className="mechanic__header"> Nazwa zakładu mechanicznego/Heading </h1>
+
+				<AppBar position="static">
+					<Tabs
+						value={value}
+						onChange={this.handleChange}
+						centered
 					>
-						<TabContainer> Usługi </TabContainer>
-          	<TabContainer> Lokalizacja </TabContainer>
-          	<TabContainer> Ocena </TabContainer>
-						
-					</SwipeableViews>
+						<Tab label="Usługi" />
+						<Tab label="Lokalizacja" />
+						<Tab label="Ocena" />
+					</Tabs>
+				</AppBar>
+
+				<SwipeableViews
+					index={this.state.value}
+					onChangeIndex={this.handleChangeIndex}
+				>
+					<TabContainer> Usługi </TabContainer>
+					<TabContainer> Lokalizacja </TabContainer>
+					<TabContainer> Ocena </TabContainer>
+
+				</SwipeableViews>
 
 				<div> Footer </div>
 			</React.Fragment>
